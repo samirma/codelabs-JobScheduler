89a951c918879c46893005fc110ceb53a568ffd2
Samir Antonio <samir.ma@gmail.com>
2020-05-24 11:11:52 +0100

    feat: add layout

--- app/src/main/java/com/codelabs/codelabs_jobscheduler/MainActivity.kt
+++ app/src/main/java/com/codelabs/codelabs_jobscheduler/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.codelabs.codelabs_jobscheduler
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mScheduler: JobScheduler? = null
+    private val JOB_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun scheduleJob(view: View) {
+        val selectedNetworkID = networkOptions.getCheckedRadioButtonId()
+        var selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
+
+        when (selectedNetworkID) {
+            R.id.noNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
+            R.id.anyNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY
+            R.id.wifiNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED
+        }
+
+        mScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler?;
+
+        val serviceName = ComponentName(
+            packageName,
+            NotificationJobService::class.java.name
+        )
+        val builder = JobInfo.Builder(JOB_ID, serviceName)
+            .setRequiredNetworkType(selectedNetworkOption);
+
+        val myJobInfo = builder.build()
+        mScheduler!!.schedule(myJobInfo)
+
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraints are met.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    fun cancelJobs(view: View) {
+        if (mScheduler!=null){
+            mScheduler?.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/codelabs/codelabs_jobscheduler/NotificationJobService.kt
+++ app/src/main/java/com/codelabs/codelabs_jobscheduler/NotificationJobService.kt
@@ -1,8 +1,15 @@
 package com.codelabs.codelabs_jobscheduler
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
 
 
 class NotificationJobService: JobService() {
@@ -17,6 +24,65 @@ class NotificationJobService: JobService() {
     }
 
     override fun onStartJob(p0: JobParameters?): Boolean {
-        TODO("Not yet implemented")
+
+        //Create the notification channel
+
+        //Create the notification channel
+        createNotificationChannel()
+
+        //Set up the notification content intent to launch the app when clicked
+
+        //Set up the notification content intent to launch the app when clicked
+        val contentPendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(
+                this,
+                MainActivity::class.java
+            ),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder =
+            NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Job Service")
+                .setContentText("Your Job ran to completion!")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true)
+
+        mNotifyManager!!.notify(0, builder.build())
+        return false
+
+    }
+
+
+    /**
+     * Creates a Notification channel, for OREO and higher.
+     */
+    fun createNotificationChannel() {
+
+        // Define notification manager object.
+        mNotifyManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.O
+        ) {
+
+            // Create the NotificationChannel with all the parameters.
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Job Service notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Notifications from Job Service"
+            mNotifyManager!!.createNotificationChannel(notificationChannel)
+        }
     }
 }
